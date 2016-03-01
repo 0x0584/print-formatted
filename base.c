9d1d26047e263738ad46e70db380941b470e3e0d
Anas Rchid <A010010001@users.noreply.github.com>
2016-03-01 05:17:31 +0000

    Update base.c

--- base.c
+++ base.c
@@ -1,3 +1,20 @@
+/*
+ * base.c
+ * This file is part of Formatted Printing in C
+ *
+ * Copyright (C) 2016 - Anas Rchid
+ *
+ * Formatted Printing in C is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Formatted Printing in C is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
 //
 //
 #include "base.h"
